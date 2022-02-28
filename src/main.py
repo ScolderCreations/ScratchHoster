3b28e244ccaa1933ce553a2ef55f675993a7d03a
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2022-02-28 13:16:55 -0800

    indent fix

--- src/main.py
+++ src/main.py
@@ -4,23 +4,23 @@ md5 = hashlib.md5()
 inpat = input("enter file path: ")
 file = open(inpat)
 try:
-  file = file.read()
+    file = file.read()
 except:
-  print("Error: file cannot be read")
-  try:
-    quit()
-  except:
-    exec('^^^^')
+    print("Error: file cannot be read")
+    try:
+      quit()
+    except:
+      exec('^^^^')
 md5.update(file)
 hash = md5.hexdigest()
 if ".png" in inpat:
-  finalhash = hash + ".png"
+    finalhash = hash + ".png"
 elif ".jpg" in inpat or "jpeg" in inpat:
-  finalhash = hash + ".jpeg"
+    finalhash = hash + ".jpeg"
 elif ".gif" in inpat:
-  finalhash = hash + ".gif"
+    finalhash = hash + ".gif"
 else:
-  print("could not find image file extension")
+    print("could not find image file extension")
 requests.post(f"https://assets.scratch.mit.edu/${finalhash}", file)
 print(f"\nhttps://assets.scratch.mit.edu/get_image/${finalhash}")
 print("\nis your link")
